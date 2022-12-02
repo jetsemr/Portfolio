70f26108e4ba9cfeca6d5557cb67e8aa8ffe5ba2
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-12-02 13:50:44 -0600

    Graphics updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/Portfolio",
+    element: <App />
+  }
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
